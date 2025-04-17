a71a287defa86020c65988a042b5c1c57ee0fc18
Tarzzth <tarzzth7@gmail.com>
2025-04-17 08:57:29 +0700

    Update LoadKey.lua

--- LoadKey.lua
+++ LoadKey.lua
@@ -1,4 +1,4 @@
-if _G.Key == "Dev by tarzzth & Flux Hub Soon" then
+if _G.Key == "Dev by tarzzth" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Tarzzth/Tool-Developer/refs/heads/main/main.lua", true))()
 else
     game:GetService("Players").LocalPlayer:Kick("Error Key ( Check Key )")
