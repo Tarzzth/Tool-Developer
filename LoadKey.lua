1ee12a33393c1f879dfebefb29366eaacff532bd
Tarzzth <tarzzth7@gmail.com>
2025-04-18 21:28:43 +0700

    Update LoadKey.lua

--- LoadKey.lua
+++ LoadKey.lua
@@ -1,5 +1,15 @@
+local Placeid = game.PlaceId
+_G.Key = "Dev by tarzzth"
 if _G.Key == "Dev by tarzzth" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tarzzth/Tool-Developer/refs/heads/main/main.lua", true))()
+    if Placeid ~= 14807448145 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Tarzzth/Tool-Developer/refs/heads/main/main.lua", true))()
+
+    else
+        game:GetService("Players").LocalPlayer:Kick("Risk Banned [ ไม่ปลอดภัยโดนแบน เสี่ยงมาก ]")
+        wait(3)
+        game:Shutdown()
+    end
+    
 else
     game:GetService("Players").LocalPlayer:Kick("Error Key ( Check Key )")
 end
